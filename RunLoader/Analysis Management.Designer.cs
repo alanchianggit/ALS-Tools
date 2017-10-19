2be2650ecdab4732262c50d5537428f65bfec05b
achianggit <achiang15@gmail.com>
2017-10-19 06:10:10 -0700

    removed entity framework and unnecessary sqllite packages

--- RunLoader/Analysis Management.Designer.cs
+++ RunLoader/Analysis Management.Designer.cs
@@ -28,19 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmb_RunNum = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // cmb_RunNum
+            // 
+            this.cmb_RunNum.FormattingEnabled = true;
+            this.cmb_RunNum.Location = new System.Drawing.Point(67, 101);
+            this.cmb_RunNum.Name = "cmb_RunNum";
+            this.cmb_RunNum.Size = new System.Drawing.Size(276, 21);
+            this.cmb_RunNum.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // Analysis_Management
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1453, 590);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmb_RunNum);
             this.Name = "Analysis_Management";
             this.Text = "Analysis_Management";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmb_RunNum;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
